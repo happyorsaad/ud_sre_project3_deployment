31a76ac233383401c54236a1198e7cbd50b3ae4e
Syed Saad Hussain <saadhussain@Saads-MacBook-Pro.local>
2022-01-13 13:16:34 +0530

    Incorporated review

--- starter/infra/_config.tf
+++ starter/infra/_config.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "udacity-tf-saadhussain" # Update here with your S3 bucket
+    bucket = "udacity-tf-ssh3" # Update here with your S3 bucket
     key    = "terraform/terraform.tfstate"
     region = "us-east-2"
     profile = "ud_nd"
